d766671cfc8db710705ddb87dd711233861778af
fu-fu <couchpo8oe@gmail.com>
2014-01-16 18:50:49 -0700

    hia

---
+++ C++/2014/charges/main.cpp
@@ -0,0 +1,22 @@
+
+
+std::list partical
+std::vector Force
+std::vector force
+std::int k = (8.9875 * (10^9))
+std::int q,Q = 1,1
+std::int r=2
+
+int main()
+{
+	while (i<partical.size()) {
+		Force=NULL
+		i++
+		while (b<partical.size()) {
+			b++
+			force = (kqQ)/(r^2)
+		}
+//		Force=force+Force	
+	cout << force.print()
+	}
+}
---
+++ C++/2014/sdl/test/test.cpp
@@ -0,0 +1,15 @@
+//using namespace STD
+#include <SDL.h>
+#include <stdio.h>
+#include <unistd.h>
+
+SDL_Window* gWindow = NULL;
+
+int main()
+{
+	SDL_Init( SDL_INIT_VIDEO );
+	gWindow = SDL_CreateWindow ("Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 600, SDL_WINDOW_SHOWN );
+	sleep(2);
+	SDL_Quit();
+}
+
